0a2f0c147d533d7a2437401318ea920df68fa93c
AdrianApan <mail@adrianapan.com>
2020-05-07 18:58:12 +0000

    Deploying to gh-pages from  @ 850cce9519875e9d923260a0fb4409bf31a1fbb3 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/npm-dependency-graph/precache-manifest.bc7476be86c5ae6eaa95cbb254a4a190.js"
+  "/npm-dependency-graph/precache-manifest.ffdcb798aae50635cb40706f01f32ee4.js"
 );
 
 self.addEventListener('message', (event) => {
